7671b9de2a90fabb3069707fb93d3c655d63ec42
Tom Webber <t.c.w.webber@gmail.com>
2026-05-05 10:08:49 +0200

    chore(lint): better lints handling

--- src/errors.rs
+++ src/errors.rs
@@ -105,7 +105,7 @@ impl Error {
                 "{repr}\nError type: {self:?}\n\x1b[3mIf you think this is a bug, please report it here: https://github.com/t-webber/fake-json/issues/new. Thanks!\x1b[0m",
             )
         } else {
-            format!("{repr}\nUse the --debug flag for more information",)
+            format!("{repr}\nUse the --debug flag for more information")
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,5 @@
 //! CLI to generate some fake data under JSON format.
 
-#![deny(
-    missing_docs,
-    warnings,
-    deprecated_safe,
-    future_incompatible,
-    keyword_idents,
-    let_underscore,
-    nonstandard_style,
-    refining_impl_trait,
-    rust_2018_compatibility,
-    rust_2018_idioms,
-    rust_2021_compatibility,
-    rust_2024_compatibility,
-    unused,
-    clippy::all,
-    clippy::pedantic,
-    clippy::style,
-    clippy::perf,
-    clippy::complexity,
-    clippy::correctness,
-    clippy::restriction,
-    clippy::nursery,
-    clippy::cargo
-)]
-#![expect(clippy::multiple_crate_versions, reason = "needed by used crates")]
-#![expect(clippy::blanket_clippy_restriction_lints, reason = "enable all lints")]
-#![expect(
-    clippy::single_call_fn,
-    clippy::implicit_return,
-    clippy::pattern_type_mismatch,
-    clippy::question_mark_used,
-    clippy::missing_trait_methods,
-    reason = "bad lints"
-)]
-#![expect(
-    clippy::mod_module_files,
-    clippy::unseparated_literal_suffix,
-    reason = "chosen style"
-)]
-#![allow(clippy::unwrap_in_result, reason = "unwrap_used is active")]
-
 mod clap;
 mod data;
 mod data_generator;
